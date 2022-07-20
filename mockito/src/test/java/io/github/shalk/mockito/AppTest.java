53244aeb3c03ca56f5d03eb956017372c3f1b5f8
shalk <xshalk@163.com>
2022-07-21 00:45:45 +0800

    first commit

---
+++ mockito/src/test/java/io/github/shalk/mockito/AppTest.java
@@ -0,0 +1,118 @@
+package io.github.shalk.mockito;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.lang.reflect.Constructor;
+
+import static org.mockito.ArgumentMatchers.anyString;
+
+class Foo {
+
+  String say() {
+    return "hi";
+  }
+
+  String echo(String a) {
+    return a;
+  }
+
+  public static String hold() {
+    return "static";
+  }
+
+  public static String reply(String a) {
+    return a;
+  }
+}
+
+public class AppTest {
+
+  @Test
+  void returnWhatIWant() {
+    Foo foo = Mockito.mock(Foo.class);
+    Mockito.when(foo.say()).thenReturn("nice to meet you");
+    String result = foo.say();
+    Assertions.assertEquals("nice to meet you", result);
+  }
+
+  @Test
+  void returnBaseOneParamOrSomething() {
+    Foo foo = Mockito.mock(Foo.class);
+    Mockito.when(foo.echo(anyString()))
+        .thenAnswer(
+            new Answer<String>() {
+              @Override
+              public String answer(InvocationOnMock invocationOnMock) throws Throwable {
+                String argument = invocationOnMock.getArgument(0, String.class);
+                return "hi," + argument;
+              }
+            });
+    String result = foo.echo("all");
+    Assertions.assertEquals("hi,all", result);
+  }
+
+  @Test
+  void verifyMethod() {
+    Foo foo = Mockito.mock(Foo.class);
+    foo.say();
+
+    Mockito.verify(foo).say();
+    Mockito.verify(foo, Mockito.times(1)).say();
+    Mockito.verify(foo, Mockito.timeout(1000)).say();
+  }
+
+  @Test
+  void mockStaticMethod() {
+    try (MockedStatic<Foo> fooMockedStatic = Mockito.mockStatic(Foo.class)) {
+      fooMockedStatic.when(() -> Foo.hold()).thenReturn("hi");
+      String result = Foo.hold();
+      Assertions.assertEquals("hi", result);
+    }
+
+    try (MockedStatic<Foo> fooMockedStatic = Mockito.mockStatic(Foo.class)) {
+      fooMockedStatic
+          .when(() -> Foo.reply(anyString()))
+          .thenAnswer(
+              new Answer<String>() {
+                @Override
+                public String answer(InvocationOnMock invocationOnMock) throws Throwable {
+                  String arg = invocationOnMock.getArgument(0, String.class);
+                  return "hi," + arg;
+                }
+              });
+      String result = Foo.reply("1024");
+      Assertions.assertEquals("hi,1024", result);
+    }
+  }
+
+  @Test
+  void mockConstruction() {
+    try (MockedConstruction<Foo> fooMockedConstruction = Mockito.mockConstruction(Foo.class)) {
+      Foo foo = new Foo();
+      foo.say();
+      Mockito.verify(foo).say();
+    }
+
+    try (MockedConstruction<Foo> fooMockedConstruction =
+        Mockito.mockConstruction(
+            Foo.class,
+            new MockedConstruction.MockInitializer<Foo>() {
+              @Override
+              public void prepare(Foo foo1, MockedConstruction.Context context) {
+                Mockito.when(foo1.say()).thenReturn("mock");
+              }
+            })) {
+      Foo foo3 = new Foo();
+      String result = foo3.say();
+      Mockito.verify(foo3, Mockito.times(1)).say();
+
+      Assertions.assertEquals("mock", result);
+    }
+  }
+}
